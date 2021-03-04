c6ed4a977f7175ea246ca9e2c47b0a04069ed462
deflatedfruit <66580940+deflated-fruit@users.noreply.github.com>
2021-03-04 18:33:05 +0000

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,9 @@
+# Simon Toogood
+# Task 1
+
+# Define distance along the screen, from -y.max to +y.max
+y.max <- 200    # Size of the screen
+n.screen <- 501 # Sets the number of pixels on the screen (ie. the resolution)
+screen <- seq(-y.max, y.max, length.out=n.screen)
+
+print(screen)
